2a762dc854d1e9b7665c93a7bdb40ee5b40fe3ba
github username <github email address>
2020-03-10 08:12:16 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import learnSymbol from './data.js'
-import Cell from './Cell.js'
-import ColorSelector from './ColorSelector.js'
+import Cell from './Cell.js' // child of the Matrix
+import ColorSelector from './ColorSelector.js' // child of the Matrix
 
 export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      selectedColor: "#FFF"
+    }
+  }
+
+  setSelectedColor = (newColor) => {
+    this.setState({selectedColor: newColor})
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />)
   )
 
   genMatrix = () => (
@@ -20,7 +27,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setSelectedColor={this.setSelectedColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
