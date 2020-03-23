05c72f2a3e03d9f9f4c9e7862e51f2886b3076b9
github username <github email address>
2020-03-22 20:25:26 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,20 +7,33 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      selectedColor: "#FFF"
+      // clickedCell: 
+    }
   }
 
+  setSelectedColor = (newColor) => {
+    console.log(this.state.selectedColor)
+    this.setState({
+      selectedColor: newColor
+    })
+  }
+  
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />)
   )
-
+  
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
   render() {
+    // console.log(this.state.selectedColor)
+
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setSelectedColor={this.setSelectedColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
@@ -32,3 +45,21 @@ export default class Matrix extends Component {
 Matrix.defaultProps = {
   values: learnSymbol
 }
+
+
+
+
+
+
+
+
+
+
+
+
+  // console.log(`you clicked ${e} bro!`)
+  // let clickedColor = "#F10" 
+  // we need to get this from ColorSelector/DOM somehow.
+  // this is a piece of logic I need to get better at.
+  // I want to pull it out of the ColorSelector directly, DOM interaction alone
+  // seems too superficial
